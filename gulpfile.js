9494fc99f432696589a4a499ed98091097396f99
Rhuan Emanuel <rhnrod@gmail.com>
2023-09-13 10:07:10 -0300

    config gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,27 @@ const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+            .pipe(sass({outputStyle: 'compressed'}))
+            .pipe(gulp.dest('./build/styles'))
+}
 
-exports.default = gulp.parallel();
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+            .pipe(uglify())
+            .pipe('./build/js')
+}
+
+function images() {
+    return gulp.src('./src/imgs/**/*')
+            .pipe(imagemin())
+            .pipe(gulp.dest('./build/imgs'))
+}
+
+exports.default = gulp.parallel(styles, scripts, images);
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
+
+}
