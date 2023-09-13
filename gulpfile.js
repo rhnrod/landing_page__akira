59055ce83eaaa490b2e389815379e64504c7a89a
Rhuan Emanuel <rhnrod@gmail.com>
2023-09-13 09:55:50 -0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+
+exports.default = gulp.parallel();
